4d179a5560932a5fad43dc193fcedb796c0c98e7
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-17 16:37:29 +0200

    done 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) resolve({ position, delay });
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.currentTarget.elements;
+  const STEP = +step.value;
+  let delayCount = +delay.value;
+
+  for (let i = 1; i <= +amount.value; i++) {
+    createPromise(i, delayCount)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayCount += STEP;
   }
 }
+
+formRef.addEventListener('submit', onSubmitForm);
